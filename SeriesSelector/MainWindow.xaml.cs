9cb882751d5a689ee23743024d060457836f58d6
TomK <tkistler@hsr.ch>
2020-06-22 13:23:49 +0200

    Daten Läuft, Buttons todo

--- SeriesSelector/MainWindow.xaml.cs
+++ SeriesSelector/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,13 @@ namespace SeriesSelector
         public MainWindow()
         {
             InitializeComponent();
+            var model = new SeriesViewModel();
+            model.SeriesList = new ObservableCollection<Series>();
+            DataContext = model;
+            var tng = new Series(@"F:\New Files\_series\Star Trek xD\TNG");
+            var twomen = new Series(@"E:\Serien\Two And A Half Men");
+            model.SeriesList.Add(tng);
+            model.SeriesList.Add(twomen);
         }
 
     }
--- SeriesSelector/Series.cs
+++ SeriesSelector/Series.cs
@@ -5,7 +5,9 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using SeriesSelector.Annotations;
 
 namespace SeriesSelector
@@ -14,14 +16,6 @@ namespace SeriesSelector
     {
         private string _filesystemPath;
         private int _currentIndex;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
 
 
         public string FilesystemPath
@@ -49,6 +43,80 @@ namespace SeriesSelector
                 _currentIndex = value;
                 OnPropertyChanged(nameof(CurrentIndex));
             }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string CurrentEpisodeAsString
+        {
+            get
+            {
+                string filename = GetFileNameOfCurrentEpisode();
+                Regex r = new Regex(@"(.* - )(\d{2}x\d{2}.*)");
+                Match m = r.Match(filename);
+                if (m.Success)
+                {
+                    return m.Groups[2].Value;
+                }
+                else
+                {
+                    return filename;
+                }
+            }
+        }
+
+        public string SeriesNameAsString
+        {
+            get
+            {
+                string[] splitted = FilesystemPath.Split('/', '\\');
+                LinkedList<string> makeMyLifeEasy = new LinkedList<string>(splitted);
+                return makeMyLifeEasy.Last.Value;
+            }
+        }
+
+        public Brush GetAColorBrush
+        {
+            get
+            {
+                try
+                {
+                    char c1 = SeriesNameAsString.ToLower()[0];
+                    char c2 = SeriesNameAsString.ToLower()[1];
+                    char c3 = SeriesNameAsString.ToLower()[2];
+
+
+                    Color c = Color.FromRgb(ScaledByteFromChar(c1), ScaledByteFromChar(c2), ScaledByteFromChar(c3));
+                    SolidColorBrush brush = new SolidColorBrush(c);
+                    return brush;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return Brushes.DeepPink;
+                }
+            }
+        }
+
+        private byte ScaledByteFromChar(char x)
+        {
+            int startFrom0 = x - 'a';
+            int rangedFrom0To250 = startFrom0 * 10;
+            int rangedFrom5To255 = 5 + rangedFrom0To250;
+            return (byte) rangedFrom5To255; //if over / underflow happens cause of spaces or numbers etc it does not matter, is just to get a color per series name.
+        }
+
+
+
+        public Series(string filepath)
+        {
+            FilesystemPath = filepath;
+            CurrentIndex = 0;
         }
 
 
